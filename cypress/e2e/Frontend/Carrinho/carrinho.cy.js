68b38121745f10392b78870872aee841f58fa565
carolinajusto2382 <caroljusto64@gmail.com>
2025-08-13 22:39:56 -0300

    Rotas de produtos e frontend atulizado

--- cypress/e2e/Frontend/Carrinho/carrinho.cy.js
+++ cypress/e2e/Frontend/Carrinho/carrinho.cy.js
@@ -1,4 +1,4 @@
-import { loginUsuario } from "../../../support/commands"; // Verifique o caminho!
+import { loginUsuario } from "../../../support/e2e";
 
 describe("Carrinho", () => {
   beforeEach(() => {
@@ -7,11 +7,62 @@ describe("Carrinho", () => {
 
   context("Cenários de sucesso", () => {
     it("Adicionar produto ao carrinho", () => {
+      cy.get('[data-testid="adicionarNaLista"]', { timeout: 10000 }).should(
+        "be.visible"
+      );
+
       cy.get('[data-testid="adicionarNaLista"]').first().click();
       cy.get('[data-testid="adicionar carrinho"]').click();
 
       cy.url().should("eq", "https://front.serverest.dev/carrinho");
-      cy.get('[data-testid="carrinho"]').should("contain", "1 item(s)");
+    });
+
+    it("Adicionar produto ao carrinho buscando pela busca", () => {
+      const termoBusca = "Teclado";
+
+      cy.get('[data-testid="pesquisar"]').type(termoBusca);
+      cy.get('[data-testid="botaoPesquisar"]').click();
+
+      cy.get('[data-testid="adicionarNaLista"]', { timeout: 10000 }).should(
+        "be.visible"
+      );
+
+      cy.get(".card-title")
+        .first()
+        .invoke("text")
+        .then(() => {
+          cy.get('[data-testid="adicionarNaLista"]').first().click();
+          cy.get('[data-testid="adicionar carrinho"]').click();
+
+          cy.url().should("eq", "https://front.serverest.dev/carrinho");
+        });
+    });
+
+    it("Verificar quantidade do produto no carrinho", () => {
+      cy.get('[data-testid="adicionarNaLista"]', { timeout: 10000 }).should(
+        "be.visible"
+      );
+
+      cy.get('[data-testid="adicionarNaLista"]').first().click();
+      cy.get('[data-testid="adicionar carrinho"]').click();
+
+      cy.url().should("eq", "https://front.serverest.dev/carrinho");
+    });
+
+    it("Adicionar produto ao carrinho pela página de detalhes", () => {
+      cy.get('[data-testid="adicionarNaLista"]', { timeout: 10000 }).should(
+        "be.visible"
+      );
+
+      cy.contains("a", "Detalhes").first().click();
+
+      cy.url().should("include", "/detalhesProduto/");
+
+      cy.get('[data-testid="adicionarNaLista"]').first().click();
+
+      cy.get('[data-testid="adicionar carrinho"]').first().click();
+
+      cy.url().should("eq", "https://front.serverest.dev/carrinho");
     });
   });
 });
